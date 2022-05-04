1e76c2d2247b5f38b493c80785447b90b412dccd
Pedro <pedrobiqua@gmail.com>
2022-05-04 09:22:45 -0300

    Melhorias

--- ArvoreAVL/Program.cs
+++ ArvoreAVL/Program.cs
@@ -10,6 +10,15 @@ namespace ArvoreAVL
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello world");
+            Console.WriteLine("Estudo de arvore AVL");
+
+            Func<int, int> square = x => x * x; //Expressão lambda antes da seta é o parametro e depois da seta é o return
+            square(5);
+
+            Console.WriteLine(square(5));
+
+            Console.ReadLine();
         }
     }
 }
