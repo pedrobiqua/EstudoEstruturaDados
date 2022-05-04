d98c75806d404bc4b04316d8107f8ff8a3553945
Pedro <pedrobiqua@gmail.com>
2022-05-04 09:14:27 -0300

    Adicionar arquivos de projeto.

---
+++ EstudoEstruturaDados/AumentaLista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoEstruturaDados
+{
+    public class AumentaLista
+    {
+        public static T[] ReajustarLista<T>(T[] arr, int valor)
+        {
+            T[] temp = new T[valor];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                temp[i] = arr[i];
+            }
+            return temp;
+        }
+    }
+}
---
+++ EstudoEstruturaDados/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoEstruturaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i] + " ");
+            }
+
+            Console.Write(" " + num);
+            Console.WriteLine();
+
+            int[] numAumentado = AumentaLista.ReajustarLista(num, num.Length + 5);
+
+            for (int i = 0; i < numAumentado.Length; i++)
+            {
+                Console.Write(numAumentado[i] + " ");
+            }
+
+            Console.Write(" " + numAumentado);
+            Console.WriteLine();
+
+            Array.Resize(ref numAumentado, numAumentado.Length + 10);
+
+            for (int i = 0; i < numAumentado.Length; i++)
+            {
+                Console.Write(numAumentado[i] + " ");
+            }
+
+            Console.Write(" " + numAumentado);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
